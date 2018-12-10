cfd09bf372c5b7142611a97a860c12af60c4090b
Simon <Simon@10.30.223.7>
2018-12-10 15:28:15 +0100

    merge conflict1

--- src/halloWelt/HalloWelt.java
+++ src/halloWelt/HalloWelt.java
@@ -3,7 +3,7 @@ package halloWelt;
 public class HalloWelt {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt");
+		System.out.println("Merch_conflict 1");
 		System.out.println("NO Yes NO");
 		System.out.println("   ");
 		System.out.println("zeile 23");
